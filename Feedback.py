f97ce65b357ca67a5c2cff5b4c758a9d5d5d6cf9
trisha <133886769+matchaprata@users.noreply.github.com>
2024-05-26 06:07:45 +0800

    Update Feedback.py

--- Feedback.py
+++ Feedback.py
@@ -5,11 +5,13 @@ st.title('Feedback')
 survey = ss.StreamlitSurvey('Feedback Form')
 pages = survey.pages(1, on_submit=lambda: st.success('Your response has been recorded. Thank you for your time!'))
 pages.submit_button = lambda pages: st.button('Submit', type='primary', use_container_width=True)
-pages.prev_button = lambda pages: None
+
 with pages:
     if pages.current == 0:
         survey.dateinput('Date:')
+        st.write(
+            'How satisfied are you with our app?')
         st.radio(
-            'How satisfied are you with our app features?', options=['😡', '☹️', '😀', '😍'], index=0, label_visibility='collapsed', horizontal=True
-        )   
+            'satisfied', options=['😡', '☹️', '😀', '😍'], index=0, label_visibility='collapsed', horizontal=True
+        )
         survey.text_area('How did we do today?')
