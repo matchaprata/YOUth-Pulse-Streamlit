368ac6962c49a1d3fa27d49460f05ea77ddfec29
matchaprata <tscl056@gmail.com>
2024-05-28 16:59:34 +0800

    updates to dropbox for feedback

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -11,18 +11,25 @@ else:
     pages = survey.pages(1, on_submit=lambda: st.success('Your response has been recorded. Thank you for your time!'))
     pages.submit_button = lambda pages: st.button('Submit', type='primary', use_container_width=True)
 
-    with pages:
-        if pages.current == 0:
-            survey.dateinput('Date:')
-            st.write(
-                'How satisfied are you with our app?')
-            st.radio(
-                'satisfied', options=['😡', '☹️', '😀', '😍'], index=0, label_visibility='collapsed', horizontal=True
-            )
-            survey.text_area('How did we do today?')
-            survey.text_area('Are there any other current affairs topics you would like to see being implemented in the discussions held?')
+with st.expander('Type of Feedback'):    
+    col1, col2, col3, col4 = st.columns(4)
+    with col1: 
+        userexp = st.button('User Experience') 
+    if userexp: 
+        with pages:
+            if pages.current == 0:
+                    survey.dateinput('Date:')
+                    st.write(
+                        'How satisfied are you with our app?')
+                    st.radio(
+                        'satisfied', options=['😡', '☹️', '😀', '😍'], index=0, label_visibility='collapsed', horizontal=True
+                    )
+                    survey.text_area('How did we do today?')
+                    survey.text_area('Are there any other current affairs topics you would like to see being implemented in the discussions held?')
 
-# connect like-minded people through collating users' interests
-
-
- 
+    with col2:
+        qna = st.button('Q&A')
+    if qna:
+        st.write(
+            'hello world'
+        )
