6cae865a0126164c80c76d9b687063744658d311
matchaprata <tscl056@gmail.com>
2024-05-29 18:38:55 +0800

    added slider under q&a for navigation

--- pages/Q&A.py
+++ pages/Q&A.py
@@ -1,2 +1,24 @@
 import streamlit as st
+from datetime import datetime
+# Custom Filter
+
+def createSection(section):
+    st.subheader(f'{section}', divider='red')
+
+
 st.title('Q&A')
+# This static filter
+tab1, tab2, tab3, tab4 = st.tabs(["Index", "Education", "Environment", f"Budget{datetime.today().year}"])
+with tab1:
+    allHeader = ['Latest', 'Most Interesting']
+    for header in allHeader:
+        createSection(header)
+with tab2:
+    allHeader = []
+    createSection("1")
+with tab3:
+    allHeader = []
+    createSection("2")
+with tab4:
+    allHeader = []
+    createSection("3")
